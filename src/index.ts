a50c9b9ce21908904122b9bd01c61cd209875ef6
lakshmipriya <lakshmipriya.mukundan@gmail.com>
2018-12-06 10:55:05 +0530

    main function added

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
-export function add(a: number, b: number) {
-  return a + b;
+import * as Slack from 'slack-node';
+
+export function savePost(post: any) {
+  const slackObj = new Slack();
+  slackObj.webhook(
+    {
+      channel: '#general',
+      username: 'webhookbot',
+      text: 'This is posted to #general and comes from a bot named webhookbot.',
+    },
+    (err, response) => {
+      if (err) {
+        console.log(err);
+      }
+    },
+  );
 }
