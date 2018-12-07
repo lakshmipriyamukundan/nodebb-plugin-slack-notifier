03ce59c18ed6d1fcf634b65bcc246159bfe1f996
lakshmipriyamukundan <lakshmipriya.mukundan@gmail.com>
2018-12-07 18:05:24 +0530

    index file change

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import * as Slack from 'slack-node';
+import { Promise as BluePromise } from 'bluebird';
+
 // import { webHookUri, channelName } from './slack-settings';
 
 const nconf = module.parent.require('nconf');
 const webHookUri = nconf.get('slackWebUrl');
 const channelName = nconf.get('channelName');
+const contentLen = nconf.get('postContentLength');
 
-export function savePost(newPost: any) {
+const user = module.parent.require('./user');
+const topics = module.parent.require('./topics');
+const categories = module.parent.require('./categories');
+
+export async function savePost(newPost: any) {
   const slackObj = new Slack();
 
   const uri = webHookUri;
@@ -13,11 +20,58 @@ export function savePost(newPost: any) {
 
   const post = newPost.post;
 
+  const userPromise = new Promise((resolve, reject) => {
+    user.getUserFields(
+      post.uid,
+      ['username', 'picture'],
+      (err: any, result: any) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve(result);
+      },
+    );
+  });
+
+  const topicPromise = new Promise((resolve, reject) => {
+    topics.getTopicFields(
+      post.tid,
+      ['title', 'slug'],
+      (err: any, result: any) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve(result);
+      },
+    );
+  });
+
+  const catPromise = new Promise((resolve, reject) => {
+    categories.getCategoryFields(
+      post.cid,
+      ['name'],
+      (err: any, result: any) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve(result);
+      },
+    );
+  });
+
+  const [userResult, topicResult, catResult] = await BluePromise.all([
+    userPromise,
+    topicPromise,
+    catPromise,
+  ]);
+
+  const content = post.content.substring(0, contentLen) + '...';
+
   slackObj.webhook(
     {
       channel: channelName || '#general',
       username: 'webhookbot',
-      text: post.content,
+      text: content,
     },
     (err, response) => {
       // tslint:disable-next-line
--- src/slack-settings.ts
+++
@@ -1,4 +0,0 @@
-export const webHookUri =
-  'https://hooks.slack.com/services/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';
-
-export const channelName = '#groot-123';
